40dc13fabf933269b3a218069701b8e8cfee0c40
Luca <luca.lucone.canessa@gmail.com>
2017-09-06 23:02:14 +0200

    update2

--- pool.c
+++ pool.c
@@ -1,14 +1,206 @@
+/******************************************************************************
+ 	 	 	 	 	 	 	 	 	 HEADER
+******************************************************************************/
 #include "./pool.h"
-#include "queue.h"
 
 
-queue_t *pool_creation( )
+
+/******************************************************************************
+ 	 	 	 	 	 	 	 	 CONDITION & LOCK
+******************************************************************************/
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+
+
+/******************************************************************************
+									FUNTIONS
+******************************************************************************/
+/* ================ SEMAPHORES ================ */
+/*
+ *
+ */
+void sem_init( sem_t *S, int val )
 {
-	queue_t *th_pool = initialQueue();
+	if( val < 0 || val > 1 )
+	{
+		fprintf( stderr, "sem_init(): binary semaphore, take only 0 or 1." );
+		exit( 1 );
+	}
 
-	for( int i = 0;)
-
-	return th_pool;
+	S->s_mutex = PTHREAD_MUTEX_INITIALIZER;
+	S->s_cond = PTHREAD_COND_INITIALIZER;
+	S->s_v = val;
 }
+
+
+
+/*
+ *
+ */
+void sem_reset( sem_t *S )
+{
+	sem_init( S, 0 );
+}
+
+
+
+/*
+ *
+ */
+void sem_post( sem_t *S )
+{
+	pthread_mutex_lock( &S->s_mutex );
+	S->s_v = 1;
+	pthread_cond_signal( &S->s_cond );
+	pthread_mutex_unlock( &S->s_mutex );
+}
+
+
+
+/*
+ *
+ */
+void every_sem_post( sem_t *S )
+{
+	pthread_mutex_lock( &S->s_mutex );
+	S->s_v = 1;
+	pthread_cond_broadcast( &S->s_cond );
+	pthread_mutex_unlock( &S->s_mutex );
+}
+
+
+
+/*
+ *
+ */
+void sem_wait( sem_t *S )
+{
+	pthread_mutex_lock( &S->s_mutex );
+	while( S->s_v != 1 )
+	{
+		pthread_cond_wait( &S->s_cond, &S->s_mutex );
+	}
+	sem->s_v = 0;
+	pthread_mutex_unlock( &S->s_mutex );
+}
+
+/* ================ JOBS ================ */
+/*
+ *
+ */
+int init_queue_jobs( queue_jobs_t *qj )
+{
+	qj->len = 0;
+	qj->head = NULL;
+	qj->tail = NULL;
+
+	qj->has_jobs = ( sem_t * )malloc( sizeof( sem_t ) );
+	if( qj->has_jobs == NULL )
+	{
+		return -1;
+	}
+
+	qj->qj_lock = PTHREAD_MUTEX_INITIALIZER;
+
+	sem_init( qj->has_jobs, 0 );
+
+	return 0;
+}
+
+
+
+/*
+ *
+ */
+void clear_jobs_queue( queue_jobs_t *qj )
+{
+	while( qj->len )
+	{
+		free( jobs_queue_push(qj) );
+	}
+
+	qj->head = NULL;
+	qj->tail = NULL;
+	sem_reset( qj->has_jobs );
+	qj->len = 0;
+}
+
+
+
+/*
+ *
+ */
+void push_jobs_queue( queue_jobs_t *qj, jobs_t *n_j )
+{
+	pthread_mutex_lock( &qj->qj_lock );
+	n_j->head = NULL;
+
+	switch( qj->len )
+	{
+		case 0:
+			qj->head = n_j;
+			qj->tail = n_j;
+			break;
+
+		default:
+			qj->tail->prev = n_j;
+			qj->tail = n_j;
+	}
+	qj->len += 1;
+
+	sem_post( qj->has_jobs );
+
+	pthread_mutex_unlock( &qj->qj_lock );
+}
+
+
+
+/*
+ *
+ */
+jobs_t *pull_jobs_queue( queue_jobs_t *qj )
+{
+	pthread_mutex_lock( &qj->qj_lock );
+	jobs_t *tmp_j = qj->head;
+
+	switch( qj->len )
+	{
+		case 0:
+			break;
+
+		case 1:
+			qj->head = NULL;
+			qj->tail = NULL;
+			qj->len = 0;
+			break;
+
+		default:
+			qj->head = tmp_j->prev;
+			qj->len -= 1;
+			sem_post( qj->has_jobs );
+	}
+
+	pthread_mutex_unlock( &qj->qj_lock );
+
+	return tmp_j;
+}
+
+
+
+/*
+ *
+ */
+void erase_jobs_queue( queue_jobs_t *qj )
+{
+	clear_jobs_queue( qj );
+	free( qj->has_jobs );
+}
+
+
+
+/*
+ *
+ */
 
 
--- pool.h
+++ pool.h
@@ -1,3 +1,11 @@
+#ifndef _POOL_H_
+#define _POOL_H_
+
+
+
+/******************************************************************************
+ 	 	 	 	 	 	 	 	 	 HEADER
+******************************************************************************/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,10 +15,70 @@
 #include <assert.h>
 
 
-typedef struct args
-{
-	int tid;
-}thread_args;
-
-
 extern int num_pool = 5;
+
+
+/******************************************************************************
+									STRUCTURES
+******************************************************************************/
+/*
+** SEMAPHORES **
+*/
+typedef struct sem
+{
+	pthread_mutex_t s_mutex;
+	phtread_cond_t s_cond;
+	int s_v;
+}sem_t;
+
+/*
+ **	JOB **
+*/
+typedef struct job
+{
+	struct job *prev;					//pointer to previous job
+	void ( * funct )( void *arg ); 		//function pointer
+	void *arg;							//pointer to function argument
+}jobs_t;
+
+
+/*
+ ** JOB QUEUE **
+*/
+typedef struct qj
+{
+	pthread_mutex_t qj_lock;			//mutex for queue access
+	jobs_t *head;						//pointer to qj queue head
+	jobs_t *tail;						//pointer to qj queue tail
+	sem_t *has_jobs;					//flag used like semaphore
+	int len;							//number of jobs in queue
+}queue_jobs_t;
+
+
+/*
+ ** THREAD POOL
+*/
+typedef struct thpool
+{
+	thread_t **threads;					//pointer to threads
+	volatile int active_th;				//number of threads at work
+	volatile int th_set;				//number of thread alive
+	pthread_mutex_t pool_lock;
+	pthread_cond_t	pool_cond;			//condition for thread to keep wait
+	queue_jobs_t *jobs;					//pointer to queue jobs
+} th_pool_t;
+
+
+/*
+ **	THREAD **
+*/
+typedef struct th
+{
+	int tid;							//thread id
+	pthread_t thread;					//pointer to thread
+	thread_pool_t *tpool;				//pointer to thread pool
+}thread_t;
+
+
+
+#endif
